f65a8ad2cb1fb0fdc9d9ed72b89ce03a59341526
bertha533 <berthaaloysia96@gmail.com>
2025-07-06 11:13:50 +0200

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import LegendDashboard from "./LegendDashboard";
-import "./index.css";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<LegendDashboard />);
